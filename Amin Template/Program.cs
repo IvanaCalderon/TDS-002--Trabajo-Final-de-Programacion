235cbc89ae78da3de8b581708a6002c0217dcd09
Ivana Calderon <ivanacalderonp@gmail.com>
2020-04-02 20:55:40 -0400

    actualice el gitignore

---
+++ Amin Template/Program.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.IO;
+
+namespace Amin_Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            class Program
+        {
+            static void Main(string[] args)
+            {
+                LogIn();
+            }
+
+            public static void LogIn()
+            {
+                Cliente cliente = new Cliente();
+
+
+                Console.WriteLine("1-Crear ticket");
+                Console.WriteLine("2-Ver historial ");
+
+                switch (int.Parse(Console.ReadLine()))
+                {
+                    case 1:
+                        Console.WriteLine("Escriba su Cedula o pasaporte");
+                        cliente.setCedulaOrPass(Console.ReadLine());
+                        Console.WriteLine("Escriba su Nombre");
+                        cliente.setNombre(Console.ReadLine());
+                        Console.WriteLine("Escriba su apellido");
+                        cliente.setApellido(Console.ReadLine());
+
+                        Consulta(cliente);
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        VerHistorial();
+                        break;
+                    default:
+                        Console.WriteLine("Opcion invalida");
+                        Thread.Sleep(4000);
+                        LogIn();
+
+                        break;
+
+                }
+
+            }
+
+            static void Consulta(Cliente cliente)
+            {
+                Console.WriteLine("1-Cambiazo");
+                Console.WriteLine("2-Reclamaciones");
+                Console.WriteLine("3-Adquirir servicios");
+                Console.WriteLine("4-Servicio al cliente");
+                switch (int.Parse(Console.ReadLine()))
+                {
+                    case 1:
+                        Ticket("Cambiazo", cliente);
+                        break;
+                    case 2:
+                        Ticket("Reclamacion", cliente);
+                        break;
+                    case 3:
+                        Ticket("Adquirir Servicios", cliente);
+                        break;
+                    case 4:
+                        Ticket("Servicio al cliente", cliente);
+                        break;
+
+                    default:
+                        Console.WriteLine("Opcion invalida");
+                        Thread.Sleep(4000);
+                        LogIn();
+
+                        break;
+                }
+
+
+            }
+            static void Ticket(string Consulta, Cliente cliente)
+            {
+                //Cliente cliente = new Cliente();
+
+                Random random = new Random();
+
+                Console.WriteLine("Numero de caja: " + random.Next(1, 20) + "                 " + DateTime.Now + "\n\n\n " + "Ticket: " + GetRandomAlfanumericTokenString(5) + "\n" + "nombre: " + cliente.getnombre() + " apellido: " + cliente.getApellido());
+
+
+
+
+                string ruta = @"C:\Users\Amin Mesa Rosario\Desktop\tickets.txt";
+
+
+                if (File.Exists(ruta))
+                {
+                    StreamWriter WriteReportFile = File.AppendText(@"C:\Users\Amin Mesa Rosario\Desktop\tickets.txt");
+                    WriteReportFile.WriteLine("Numero de caja: " + random.Next(0, 20) + "                 " + DateTime.Now + "\n\n\n " + "Ticket: " + GetRandomAlfanumericTokenString(5) + "\n" + "nombre: " + cliente.getnombre() + " apellido: " + cliente.getApellido());
+                    WriteReportFile.Close();
+                }
+                else
+                {
+                    using (FileStream fs = File.Create(ruta)) ;
+
+                    StreamWriter WriteReportFile = File.AppendText(@"C:\Users\Amin Mesa Rosario\Desktop\tickets.txt");
+                    WriteReportFile.WriteLine("Numero de caja: " + random.Next(0, 20) + "                 " + DateTime.Now + "\n\n\n " + "Ticket: " + GetRandomAlfanumericTokenString(5) + "\n" + "nombre: " + cliente.getnombre() + " apellido: " + cliente.getApellido());
+                    WriteReportFile.Close();
+                }
+
+                Console.WriteLine("Ticket creado correctamente volviendo al menu ");
+                Thread.Sleep(4000);
+                Console.Clear();
+                LogIn();
+            }
+
+            static void VerHistorial()
+            {
+                string ruta = @"C:\Users\Amin Mesa Rosario\Desktop\tickets.txt";
+                if (!File.Exists(ruta))
+                {
+                    Console.WriteLine("El archivo fue creado pero no contiene logs");
+                    using (FileStream fs = File.Create(ruta)) ;
+
+
+                    Thread.Sleep(4000);
+                    //Console.Clear();
+                    LogIn();
+                }
+
+                else
+                {
+                    int counter = 0;
+                    string line;
+                    StreamReader file = new StreamReader(@"C:\Users\Amin Mesa Rosario\Desktop\tickets.txt");
+
+
+                    while ((line = file.ReadLine()) != null)
+                    {
+
+                        Console.WriteLine(line);
+                        Console.WriteLine(Environment.NewLine);
+                        counter++;
+                    }
+
+                    string check = line = file.ReadLine();
+
+                    if (check == null & counter == 0)
+                    {
+                        Console.WriteLine("El archivo no tiene  mas registros");
+                    }
+
+
+
+
+                    file.Close();
+
+                    Thread.Sleep(4000);
+                    Console.Clear();
+                    LogIn();
+                }
+
+
+            }
+
+
+            public static string GetRandomAlfanumericTokenString(int length)
+            {
+
+                Random rnd = new Random();
+                string charPool = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+                StringBuilder rs = new StringBuilder();
+
+                while (length > 0)
+                {
+                    rs.Append(charPool[(int)(rnd.NextDouble() * charPool.Length)]);
+                    length--;
+                }
+
+
+                return rs.ToString();
+
+
+            }
+
+        }
+        class Cliente
+        {
+            public string cedulaOrPass { get; set; }
+
+            public string nombre { get; set; }
+
+            public string apellido { get; set; }
+
+
+            //----------------------
+            public string getCedulaorPass()
+            {
+                return this.cedulaOrPass;
+            }
+
+            public void setCedulaOrPass(string cedulaOrPass)
+            {
+                this.cedulaOrPass = cedulaOrPass;
+            }
+            //------------------------
+            public string getnombre()
+            {
+                return this.nombre;
+            }
+
+            public void setNombre(string nombre)
+            {
+                this.nombre = nombre;
+            }
+            //------------------------
+            public string getApellido()
+            {
+                return this.apellido;
+            }
+
+            public void setApellido(string apellido)
+            {
+                this.apellido = apellido;
+            }
+
+
+
+        }
+    }
+}
---
+++ Proyecto_final_de_programacion/Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Proyecto_final_de_programacion
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+
+
+
+
+            string loginAdmin = @"C:\demos\demo.txt";
+            
+            string[] lines = File.ReadAllLines(loginAdmin);
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.WriteLine(lines[0]);
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+            static void menuInicio() //Menu que aparece al iniciar la aplicacion
+            {
+                Console.WriteLine("Bienvenido al sistema de alquiler de peliculas");
+                Console.WriteLine("Ingrese el numero para el tipo de usuario que quiere acceder");
+                Console.WriteLine("[1] - Administrador");
+                Console.WriteLine("[2] - Empleado");
+                Console.WriteLine("[3] - Cliente");
+
+                switch (int.Parse(Console.ReadLine()))
+                {
+                    case 1:
+                        loginAdministador();
+                        break;
+                    case 2:
+                        loginEmpleado();
+                        break;
+                    case 3:
+                        loginCiente();
+                        break;
+                    default:
+                        menuInicio();
+                        break;
+
+                }
+
+            }
+
+            static void loginCiente() //Login de los clientes
+            {
+                Console.WriteLine("Ingresa  tu nombre de usuario");
+                Console.ReadLine();
+                Console.WriteLine("Ingresa tu contraseña");
+                Console.ReadLine();
+
+
+            }
+
+
+            static void loginEmpleado() //Login de los epleados
+            {
+                Console.WriteLine("Ingresa  tu nombre de usuario");
+                Console.ReadLine();
+                Console.WriteLine("Ingresa tu contraseña");
+                Console.ReadLine();
+
+
+            }
+
+            static void loginAdministador() //Login del Administrador
+            {
+                String loginAdmin = @"C:\Users\Lisa\source\repos\Proyecto_final_de_programacion";
+
+                Console.WriteLine("Ingresa  tu nombre de usuario");
+                Console.ReadLine();
+                Console.WriteLine("Ingresa tu contraseña");
+                Console.ReadLine();
+
+
+            }
+
+            static void menuAdministrador() //Menu del administrador
+            {
+                Console.WriteLine("Elige la accion que quieres:");
+                Console.WriteLine("[1] - Agregar empleado");
+                Console.WriteLine("[2] - Editar empleado");
+                Console.WriteLine("[3] - Borrar empleado");
+                Console.WriteLine("[4] - Agregar pelicula");
+                Console.WriteLine("[5] - Editar pelicula");
+                Console.WriteLine("[6] - Borrar pelicula");
+                Console.WriteLine("[7] - Ganancias de las peliculas");
+            }
+
+
+            static void menuCliente() // Menu del Cliente
+            {
+                Console.WriteLine("Elige la accion que quieres:");
+                Console.WriteLine("[1] - Alquilar pelicula");
+                Console.WriteLine("[2] - Lista de peliculas alquiladas");
+                Console.WriteLine("[3] - Fecha de entrega de pelicula");
+
+            }
+
+            static void menuEmpleado() // Menu del empleado
+            {
+                Console.WriteLine("Elige la accion que quieres:");
+                Console.WriteLine("[1] - Agregar cliente");
+                Console.WriteLine("[2] - Editar cliente");
+                Console.WriteLine("[3] - Alquilar pelicula a cliente");
+                Console.WriteLine("[4] - Peliculas alquiladas");
+            }
+
+
+            static void agregarEmpleado() //jjj
+            {
+                Console.WriteLine("Escribe nuevo nombre de usuario: ");
+                Console.ReadLine();
+                Console.WriteLine("Asigna contraseña al usuario: ");
+                Console.ReadLine();
+                Console.WriteLine("Escribe primer nombre:");
+                Console.ReadLine();
+                Console.WriteLine("Escribe apellido: ");
+                Console.ReadLine();
+                Console.WriteLine("Escribe direccion: ");
+                Console.ReadLine();
+                Console.WriteLine("Escribe el cargo: ");
+
+        }   }
+           
+
+
+            
+          
+
+
+
+        class Administrador
+        {
+            public string Nombre { get; set; }
+
+            public string Apellido { get; set; }
+
+            public string Direccion { get; set; }
+
+            public string Cargo { get; set; }
+
+        }
+
+        class Empleado
+        {
+            public string Nombre { get; set; }
+
+            public string Apellido { get; set; }
+
+            public string Direccion { get; set; }
+
+            public string Cargo { get; set; }
+
+        }
+
+        class Cliente
+        {
+            public string Nombre { get; set; }
+
+            public string Apellido { get; set; }
+
+            public string Direccion { get; set; }
+
+          
+
+        }
+
+        class nanana { }
+
+        
+
+
+    }
+}
